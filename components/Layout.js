ff391a166dd1c51c22c6316b4a455b7314f8caf3
ChuaWeiHan0924 <81673644+ChuaWeiHan0924@users.noreply.github.com>
2023-09-23 17:50:14 +0800

    changed the theme of the page
    damn black theme hits alot ser

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import dynamic from 'next/dynamic';
 const RootLayout = ({ children }) => {
   return (
       
-        <div className="flex flex-col min-h-screen mx-auto max-w-7xl px-4 pt-8 pb-16">
+        <div className="flex flex-col min-h-screen mx-auto max-w-7xl">
           <Header />
           <div className="flex flex-col min-h-screen mx-auto max-w-5xl px-4">
             <main className="my-0">{children}</main>
--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 function Home() {
 
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
+    <main className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}>
 
-      <div className="relative flex flex-col justify-between place-items-center">
+      <div className="relative flex flex-col justify-between place-items-center p-12">
         <LandingPageContents />
       </div>
       
