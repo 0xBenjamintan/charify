2c697c85d5c0c39d8b98043d96e2314902e5c0c3
Benjamin <tankerkzheng1@gmail.com>
2023-09-23 11:03:31 +0800

    change node provider

--- components/WalletConnectProvider.js
+++ components/WalletConnectProvider.js
@@ -1,32 +1,32 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { FC, useMemo } from 'react';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
+import React, { FC, useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
 
 export const WalletConnectProvider = ({ children }) => {
-    const network = WalletAdapterNetwork.Devnet
+  const network = WalletAdapterNetwork.Devnet;
 
-    const endpoint = useMemo (() => {
-        if (network === WalletAdapterNetwork.Devnet) {
-            return 'https://fragrant-wider-dawn.solana-devnet.discover.quiknode.pro/e28bd823c1b8e232f77b0d36425d13b00e22c4f9/'
-        }
-
-        return clusterApiUrl(network)
+  const endpoint = useMemo(() => {
+    if (network === WalletAdapterNetwork.Devnet) {
+      return "https://solana-devnet.g.alchemy.com/v2/MiucvWotH4CAcYdoKMWUmLljTBXuvJrV";
     }
-    , [network])
 
-    const wallets = useMemo (() => [new PhantomWalletAdapter()], [network])
+    return clusterApiUrl(network);
+  }, [network]);
 
-    return (
-        <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets} autoConnect>
-                <WalletModalProvider>
-                    {children}
-                </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
-    )
-}
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
