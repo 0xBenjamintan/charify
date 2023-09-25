c445e76b94f5b610ac59a0de2c782c22a3b5f0c7
Benjamin <tankerkzheng1@gmail.com>
2023-09-25 21:28:07 +0800

    add new logo

--- components/LandingPageContents.js
+++ components/LandingPageContents.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
+import { Model } from "@/components/Scene";
 
 const LandingPageContents = ({ imageUrl, title }) => {
   return (
@@ -7,7 +10,17 @@ const LandingPageContents = ({ imageUrl, title }) => {
         <div className="md:w-1/2 pr-4">
           {/* Left Column (Image) */}
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="./job.png" alt="Image" className="w-full" />
+          {/* <img src="./job.png" alt="Image" className="w-full" /> */}
+          <Canvas>
+            <PerspectiveCamera position={[0, 0, 5]} makeDefault={true} />
+            <Suspense fallback={null}>
+              <ambientLight intensity={0.5} />
+              <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+              <pointLight position={[-10, -10, -10]} />
+              <Model position={[0, -1, 0]} scale={[1.25, 1.25, 1.25]} />
+              <OrbitControls target={[0, 0, 0]} enableZoom={false} />
+            </Suspense>
+          </Canvas>
         </div>
         <div className="md:w-1/2 mt-4 md:mt-0 flex flex-col justify-center">
           {/* Right Column (Title) */}
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
-import { Inter } from 'next/font/google'
-import dynamic from 'next/dynamic'
-import LandingPageContents from '@/components/LandingPageContents';
+import { Inter } from "next/font/google";
+import dynamic from "next/dynamic";
+import LandingPageContents from "@/components/LandingPageContents";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 function Home() {
-
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}>
-
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
+    >
       <div className="relative flex flex-col justify-between place-items-center mt-10">
         <LandingPageContents />
       </div>
-      
     </main>
-  )
+  );
 }
 
-export default dynamic (() => Promise.resolve(Home), {ssr: false} )
+export default dynamic(() => Promise.resolve(Home), { ssr: false });
