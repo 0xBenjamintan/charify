2518eca6a4dcf14c60e1bfe4b81412795074f0cf
Benjamin <tankerkzheng1@gmail.com>
2023-09-27 22:15:27 +0800

    done page login

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
-import { Inter } from "next/font/google";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import dynamic from "next/dynamic";
 import LandingPageContents from "@/components/LandingPageContents";
 import ProductDetails from "@/components/ProductDetails";
-import Head from 'next/head'
+import Head from "next/head";
+import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function Home() {
+  const { publicKey } = useWallet();
+  const router = useRouter();
+
+  useEffect(() => {
+    // Check if publicKey is present and redirect accordingly
+    if (publicKey) {
+      router.push("/mainPage"); // Replace with the actual path to mainpage.js
+    }
+  }, [publicKey, router]);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
--- pages/mainPage.js
+++ pages/mainPage.js
@@ -4,15 +4,11 @@ import NftContainer from "@/components/NftContainer";
 
 function mainPage() {
   return (
-    <div>
-      <main
-        className={`min-h-screen items-center justify-between`}
-      >
-        <div className="relative justify-between place-items-center mt-10">
-          <NftContainer />
-        </div>
-      </main>
-    </div>
+    <main className={`min-h-screen items-center justify-between`}>
+      <div className="relative justify-between place-items-center mt-10">
+        <NftContainer />
+      </div>
+    </main>
   );
 }
 
