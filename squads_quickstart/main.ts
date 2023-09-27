332ee8b14cd035185dae5b271596f155fa5a003f
Terry Lau <terrylau0708@gmail.com>
2023-09-27 20:19:08 +0800

    feat: made changes to main.ts

--- squads_quickstart/main.ts
+++ squads_quickstart/main.ts
@@ -1,5 +1,5 @@
 import * as multisig from "@sqds/multisig";
-import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { Connection, Keypair, LAMPORTS_PER_SOL, SystemProgram, TransactionMessage, TransferParams } from "@solana/web3.js";
 
 const { Permission, Permissions } = multisig.types;
 const connection = new Connection("http://localhost:8899", "confirmed");
@@ -48,50 +48,95 @@ describe("Interacting with the Squads V4 SDK", () => {
         });
         console.log("Multisig created: ", signature);
     });
-});
 
+    // Create transaction proposal
+    it("Create a transaction proposal", async () => {
+        const [vaultPda, vaultBump] = multisig.getVaultPda({
+            multisigPda,
+            index: 0,
+        });
 
-it("Create a transaction proposal", async () => {
-    const [vaultPda, vaultBump] = multisig.getVaultPda({
-        multisigPda,
-        index: 0,
-    });
-    const instruction = SystemProgram.transfer(
-    // The transfer is being signed from the Squads Vault, that is why we use the VaultPda
-        vaultPda,
-        creator.publicKey,
-        1 * LAMPORTS_PER_SOL
-    );
-    // This message contains the instructions that the transaction is going to execute
-    const transferMessage = new TransactionMessage({
-        payerKey: vaultPda,
-        recentBlockhash: (await connection.getLatestBlockhash()).blockhash,
-        instructions: [instruction],
-    });
-    // This is the first transaction in the multisig
-    const transactionIndex = 1n;
-    const signature1 = await multisig.rpc.vaultTransactionCreate({
-        connection,
-        feePayer: creator,
-        multisigPda,
-        transactionIndex,
-        creator: creator.publicKey,
-        vaultIndex: 0,
-        ephemeralSigners: 0,
-        transactionMessage: transferMessage,
-        memo: "Transfer 0.1 SOL to creator",
+        const transferParam : TransferParams = {
+            fromPubkey: vaultPda,
+            toPubkey: creator.publicKey,
+            lamports: 1 * LAMPORTS_PER_SOL
+        };
+
+        const instruction = SystemProgram.transfer(
+        // The transfer is being signed from the Squads Vault, that is why we use the VaultPda
+            transferParam
+        );
+
+        // This message contains the instructions that the transaction is going to execute
+        const transferMessage = new TransactionMessage({
+            payerKey: vaultPda,
+            recentBlockhash: (await connection.getLatestBlockhash()).blockhash,
+            instructions: [instruction],
+        });
+        // This is the first transaction in the multisig
+        const transactionIndex = BigInt(1);
+        const signature1 = await multisig.rpc.vaultTransactionCreate({
+            connection,
+            feePayer: creator,
+            multisigPda,
+            transactionIndex,
+            creator: creator.publicKey,
+            vaultIndex: 0,
+            ephemeralSigners: 0,
+            transactionMessage: transferMessage,
+            memo: "Transfer 0.1 SOL to creator",
+        });
+
+        console.log("Transaction created: ", signature1);
+        
+        const signature2 = await multisig.rpc.proposalCreate({
+            connection,
+            feePayer: secondMember,
+            multisigPda,
+            transactionIndex,
+            creator: creator,
+        });
+        
+        console.log("Transaction proposal created: ", signature2);
+        
     });
 
-    console.log("Transaction created: ", signature1);
-    
-    const signature2 = await multisig.rpc.proposalCreate({
-        connection,
-        feePayer: members.voter,
-        multisigPda,
-        transactionIndex,
-        creator: feePayer,
+    // Vote on the created proposal
+    it("Vote on the created proposal", async () => {
+        const transactionIndex = BigInt(1);
+        multisig.rpc.proposalApprove({
+            connection,
+            feePayer: creator,
+            multisigPda,
+            transactionIndex,
+            member: creator,
+        });
+   
+        multisig.rpc.proposalApprove({
+            connection,
+            feePayer: creator,
+            multisigPda,
+            transactionIndex,
+            member: secondMember,
+            // signers: [creator, secondMember],
+        });
     });
-    
-    console.log("Transaction proposal created: ", signature2);
-    
+
+    // Execute the proposal
+    it("Execute the proposal", async () => {
+        const transactionIndex = BigInt(1);
+        const [proposalPda] = multisig.getProposalPda({
+            multisigPda,
+            transactionIndex,
+        });
+        const signature = await multisig.rpc.vaultTransactionExecute({
+            connection,
+            feePayer: creator,
+            multisigPda,
+            transactionIndex,
+            member: creator.publicKey,
+            signers: [creator],
+        });
+        console.log("Transaction executed: ", signature);
+    });
 });
