b7c88f6cf1ee4c808ffff42f66647d235408ac4f
ChuaWeiHan0924 <81673644+ChuaWeiHan0924@users.noreply.github.com>
2023-09-28 18:32:25 +0800

    pushed css

--- components/Card.js
+++ components/Card.js
@@ -3,10 +3,8 @@ import PropTypes from "prop-types";
 
 const BlurCard = ({ children }) => {
   return (
-    <div className="relative">
-      <div className="absolute w-96 h-96 bg-slate-50/20 backdrop-blur-lg rounded-lg shadow-md p-4 mx-4 my-4">
-        {children}
-      </div>
+    <div className="relative w-96 h-96 bg-slate-50/20 backdrop-blur-lg rounded-lg shadow-md p-4 mx-4 my-4 z-50 governanceCard">
+      {children}
     </div>
   );
 };
